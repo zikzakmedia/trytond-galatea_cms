8b6906cf25a65f005fbf57b0768cdaa2b77153a5
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:24:08 +0100

    Cleanup tests

--- tests/test_galatea_cms.py
+++ tests/test_galatea_cms.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python
-#This file is part galatea_cms module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part galatea_cms module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class GalateaCmsTestCase(unittest.TestCase):
-    '''
-    Test Galatea CMS module.
-    '''
+    'Test Galatea CMS module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('galatea_cms')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('galatea_cms')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
